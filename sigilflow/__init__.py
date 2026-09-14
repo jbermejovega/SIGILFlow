711b763d6a922501c4c3c81b9e80328e6759cded
Jara Juana (Juani) Bermejo-Vega (she/her) <75273221+jbermejovega@users.noreply.github.com>
2026-09-14 15:10:59 +0200

    Expose SIGILFlow kernel API

---
+++ sigilflow/__init__.py
@@ -0,0 +1,25 @@
+from .kernel import (
+    SCHEMA_ID,
+    TFG_REPO,
+    TFG_COMMIT,
+    SIGILBOOK_REPO,
+    SIGILBOOK_COMMIT,
+    SIGILFLOW_REPO,
+    SourceRole,
+    RepoPin,
+    FlowLibrary,
+    Section,
+    RestrictionMap,
+    Presheaf,
+    SheafPreimage,
+    JoinMeetFusion,
+    SyncPolicy,
+    SigilFlowKernel,
+    build_reference_kernel,
+)
+
+__all__ = [
+    "SCHEMA_ID", "TFG_REPO", "TFG_COMMIT", "SIGILBOOK_REPO", "SIGILBOOK_COMMIT", "SIGILFLOW_REPO",
+    "SourceRole", "RepoPin", "FlowLibrary", "Section", "RestrictionMap", "Presheaf", "SheafPreimage",
+    "JoinMeetFusion", "SyncPolicy", "SigilFlowKernel", "build_reference_kernel",
+]
